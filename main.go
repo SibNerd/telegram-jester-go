f8e157dbf0b1f1e7ed7f1f1c6e98b4fea413d02e
Irina Yaremchuk <54741988+SibNerd@users.noreply.github.com>
2021-08-16 17:05:40 +0000

    Add jokes handling

--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-  "fmt"
+  "net/http"
+  "encoding/json"
   "github.com/Syfaro/telegram-bot-api"
+  "github.com/icelain/jokeapi"
   "log"
   "io/ioutil"
   )
 
+// const joke_api_one := "https://official-joke-api.appspot.com/jokes/general/random"
+
+func getJson(url string, target interface{}) error {
+  // Get nice JSON from URL response
+  var myClient = &http.Client{Timeout: 10 * time.Second}
+    r, err := myClient.Get(url)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+
+    return json.NewDecoder(r.Body).Decode(target)
+}
+
 func main() {
+  // Set parameters for jokeAPI
+  joke_type := "single"
+  blacklist := []string{"rasist", "sexist"}
+  ctgs := []string{"Misc", "Pun", "Dark"}
+  
   // Get bot token from separate file
   bot_token, err := ioutil.ReadFile("token.txt")
     if err != nil {
           log.Fatal(err)
      }
+  
+  // Connect to API with jokes
+  joke_api, err := jokeapi.New()
+  
+  if err != nil {
+  	panic(err)
+  }
   
   // Create bot   
 	bot, err := tgbotapi.NewBotAPI(string(bot_token)) 
@@ -36,12 +64,21 @@ func main() {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
+    
+    if update.Message.IsCommand() {
+      log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+      
+      msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+      switch update.Message.Command() {
+        case "help": 
+          msg.Text("Выберите команду /joke для получения случайной шутки.")
+        case "joke":
+          api.SetParams(&ctgs, &blacklist, &jt)
+          response := api.Fetch()
+          msg.Text(response)
+        default: 
+          msg.Text("Боюсь, я не знаю такой команды :с")
+      }
 		bot.Send(msg)
 	}
 }
