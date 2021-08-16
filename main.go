9ee9875cdaa237c96dd8cce0afde864bf6c5705e
Irina Yaremchuk <54741988+SibNerd@users.noreply.github.com>
2021-08-16 13:23:54 +0000

    Add basic bot functionality

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/Syfaro/telegram-bot-api"
+  "log"
+  "ioutil"
+  )
 
 func main() {
-  fmt.Println("Hello World")
+  // Get bot token from separate file
+  bot_token, err := ioutil.ReadFile("token.txt")
+    if err != nil {
+          log.Fatal(err)
+     }
+  
+  // Create bot   
+	bot, err := tgbotapi.NewBotAPI(bot_token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+	  log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
